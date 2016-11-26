204b400102c2edf733e1bc6f21e35eb4a9225724
natsu1211 <longyutao1211@gmail.com>
2016-11-27 01:09:41 +0900

    use RxSwift 3

---
+++ Weather-Alarm-RxSwift/Pods/Target Support Files/Pods-test-Weather-Alarm-RxSwiftTests/Pods-test-Weather-Alarm-RxSwiftTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_test_Weather_Alarm_RxSwiftTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_test_Weather_Alarm_RxSwiftTestsVersionString[];
+
---
+++ Weather-Alarm-RxSwift/Pods/Target Support Files/Pods-test-Weather-Alarm-RxSwiftUITests/Pods-test-Weather-Alarm-RxSwiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_test_Weather_Alarm_RxSwiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_test_Weather_Alarm_RxSwiftUITests
+@end
