dfa365f50e3e08b24fa64b375317299bc8205bf5
natsu1211 <longyutao1211@gmail.com>
2016-11-26 22:16:43 +0900

    add alamofire using cocoapods

---
+++ Weather-Alarm-RxSwift/Pods/Target Support Files/Pods-Weather-Alarm-RxSwift/Pods-Weather-Alarm-RxSwift-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Weather_Alarm_RxSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Weather_Alarm_RxSwiftVersionString[];
+
